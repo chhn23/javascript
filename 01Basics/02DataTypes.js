910a68471817859d3123e562cec9c10a770d176d
chhn23 <dpksnghchhn23@gmail.com>
2024-10-01 07:20:20 +0000

    data  types

---
+++ 01Basics/02DataTypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all code as new version
+//alert(3+3)// it wont work as we are using node not browser
+let name='Deepak'
+let age=19;
+let isLoggedIn=true;
+
+//number 2 to the power 53
+//String
+//bigInt
+//boolean
+//null  => standalone value
+//undefined
+//symbol => unique
+
+
+//object
+
+console.log(typeof null);       //object
+console.log(typeof undefined);  //undefined
