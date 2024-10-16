0c326c636c2e0e18862460a2b2301a17944ab146
chhn23 <dpksnghchhn23@gmail.com>
2024-10-16 18:01:59 +0000

    objects

---
+++ 02_Basics/04Objects.js
@@ -0,0 +1,39 @@
+//const user= new Object();
+let user={
+    "email":"something@gmail.com",
+    "fullNAme":"fname"
+}
+console.log(user);
+const regularUser={
+    "email":"something@gmail.com",
+    "fullNAme":{
+        "userfullName":{
+            "firstName":"Deepak",
+            "lastName":"Chauhan"
+        }
+    }
+};
+console.log(regularUser.fullNAme.userfullName.firstName)
+const obj1={
+    1:"a",
+    2:"b"
+};
+const obj2={
+    3:"c",
+    4:"d"
+}
+//const obj3={obj1,obj2}
+//const obj3=Object.assign({},obj1,obj2)
+const obj3={...obj1,...obj2}
+console.log(obj3)
+const users=[{
+    "id":1,
+    "name":"n1"
+},{
+    "id":2,
+    "name":"n2"
+}]
+console.log(Object.keys(user))
+console.log(Object.values(user))
+console.log(Object.entries(user))
+console.log(user.hasOwnProperty('isloggedin'))
